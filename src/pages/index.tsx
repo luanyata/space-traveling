f0947c78369440c1c1f32f4071d3ae5511e0d3d1
Luan Lima <luan@yata.dev>
2021-04-17 14:09:40 -0300

    :sparkles:  Adicao da listagem dos posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-
+import Prismic from '@prismicio/client'
 import { getPrismicClient } from '../services/prismic';
-
+import { FiCalendar, FiUser } from "react-icons/fi";
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Link from 'next/link';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useCallback, useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -25,18 +29,93 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): any {
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState(postsPagination.results);
+
+
+
+  const handleNextPosts = useCallback(async () => {
+
+    try{
+      const response = await fetch(postsPagination.next_page);
+      const { results } = await response.json();
+      const newPostsArray = [...posts, ...results]
+      setPosts(newPostsArray);
+    }catch{
+      alert('Ocorreu um erro na recuperacao dos novos posts')
+    }
+
+  }, [])
+
   return (
     <>
       <Head>Home</Head>
-      <main>Lima</main>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          {postsPagination && postsPagination.results.map(post =>
+            <Link href={`/posts/${post.uid}`} key={post.uid}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+
+
+                <div>
+                  <FiCalendar size={18} />
+                  <span>{format(new Date(post.first_publication_date), 'PP', {
+                    locale: ptBR,
+                  })}
+                  </span>
+                  <FiUser size={18} />
+                  <span>
+                    {post.data.author}
+                  </span>
+                </div>
+
+              </a>
+            </Link>
+          )}
+
+          {postsPagination.next_page && (
+            <button
+              type="button"
+              onClick={handleNextPosts}>
+              Carregar mais posts
+            </button>
+          )}
+        </div>
+      </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+    }
+  );
 
-//   // TODO
-// };
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+      first_publication_date: post.first_publication_date,
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results,
+      },
+    },
+  };
+};
